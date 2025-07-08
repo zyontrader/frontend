998507a78a93b4c845539557c217a2bb4b2528c7
Ritik Shekhar Parida <ritikshekharparida2002@gmail.com>
2025-07-08 14:19:02 +0530

    fix: Correct casing of ContactUs component and update import statement for consistency

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function Contactus() {
+function ContactUs() {
   return (
-    <div>Contactus</div>
+    <div>ContactUs</div>
   )
 }
 
-export default Contactus
+export default ContactUs
